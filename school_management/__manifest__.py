df81312e8e7f4dde0d75009edc4a33d4dcb92ccb
odoo <Aktivsoftware@123.com>
2020-09-19 17:50:15 +0530

    [Add]adding in intial version of odoo13

--- school_management/__manifest__.py
+++ school_management/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Aktiv software",
     'website': "http://www.aktivesoftware.com",
     'category': 'Tooles',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
